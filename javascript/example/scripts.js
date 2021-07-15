58d6317a9c3189408bfcf8f6856f1ede0b648bf9
Kevin Zhang <kevinleezhang@gmail.com>
2021-07-15 16:30:04 -0400

    MVP for buttons

--- javascript/example/scripts.js
+++ javascript/example/scripts.js
@@ -10,19 +10,45 @@ ros.on('close', function() {document.getElementById("status").innerHTML = "Close
 
 var display_msg_listener = new ROSLIB.Topic({ros : ros,name : "/mock_human_interface_publisher",messageType : 'domain_handler_msgs/HumanInterfaceRequest'});
 
-function log_info(val) {
-  console.log(val)
-              move(val)
-            }
+var state_server_publisher = new ROSLIB.Topic({ros : ros,name : "/state_server",messageType : 'domain_handler_msgs/HumanInterfaceReply'});
+
+var domain_handler_publisher = new ROSLIB.Topic({ros : ros,name : "/domain_handler",messageType : 'std_msgs/Int32'});
+
+
+let viz_data;
+
+function button_click(i){
+  console.log("button_pressed");
+  new_buttons_data  = viz_data.buttons.slice();
+  new_buttons_data[i].value = true;
+
+  var return_msg = new ROSLIB.Message({
+      buttons: new_buttons_data,
+      sliders: viz_data.sliders,
+      text_inputs: viz_data.text_inputs,
+      Bbox: viz_data.boxes
+    });
+
+  state_server_publisher.publish(return_msg);
+
+  var confirmation_msg = new ROSLIB.Message({data: 1});
+  domain_handler_publisher.publish(confirmation_msg);
+
+  console.log(return_msg)
+
+  clear_screen();
+}
+
+
 
 function generate_buttons(buttons_array){
   for(let i = 0;i < buttons_array.length;i++){
     var button = document.createElement('input');
     button.type = 'button';
-    button.id = buttons_array[i];
-    button.value = buttons_array[i];
+    button.id = buttons_array[i].text;
+    button.value = buttons_array[i].name;
     button.className = 'button';
-    button.onclick = function() {log_info(i);};
+    button.onclick = function() {button_click(i);};
  
     var container = document.getElementById('button_container');
     container.appendChild(button);
@@ -57,8 +83,12 @@ function generate_sliders(sliders_array){
   }
 }
 
-function parse_screen(data){
-  console.log(data)
+function parse_data(data){
+  if (data.display_type == 0 ){
+    viz_data = data
+    document.getElementById("msg").innerHTML = data.instruction_text;
+    generate_buttons(data.buttons)
+  }
 }
 
 function display_default_screen(){
@@ -76,20 +106,27 @@ function display_default_screen(){
   robot_canvas.id = "robot_canvas";
   robot_canvas.className = "robot_viz";
 
-  var container = document.getElementById('visuals_container');
+  var container = document.getElementById('row');
   //container.appendChild(robot_viz_div);
   //
-  container.appendChild(robot_viz);
   container.appendChild(robot_canvas);
+  container.appendChild(robot_viz);
+  
 
   container.appendChild(camera_div);
 }
 
+function clear_screen(){
+  document.getElementById("msg").innerHTML = "";
+  document.getElementById('button_container').innerHTML = "";
+}
+
+console.log("listening to data")
 display_msg_listener.subscribe(function(data) {
-            parse_screen(data);
+            parse_data(data);
         });
 
 display_default_screen();
-generate_buttons(["hi","bye","option 1","option 2","option 3"]);
+//generate_buttons(["hi","bye","option 1","option 2","option 3"]);
 //generate_sliders([["slider_1",1,10],["slider_dedewd2",5,100]]);
 console.log("executed creating buttons");
